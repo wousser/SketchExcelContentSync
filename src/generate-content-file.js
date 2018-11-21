759e29ede3c40fe96222b507f59f3e5678d38e12
wousser <wousser@gmail.com>
2018-11-22 00:24:25 +0800

    Update saveFile method and test with layer ID

--- src/generate-content-file.js
+++ src/generate-content-file.js
@@ -1,7 +1,6 @@
 import sketch from 'sketch'
 import fs from '@skpm/fs'
 import dialog from '@skpm/dialog'
-const utilities = require('./utilities')
 const constants = require('./constants')
 const path = require('path')
 const XLSX = require('xlsx')
@@ -10,17 +9,13 @@ const XLSX = require('xlsx')
 // Based on: https://github.com/DWilliames/Google-sheets-content-sync-sketch-plugin/blob/master/Google%20sheets%20content%20sync.sketchplugin/Contents/Sketch/main.js
 
 const document = sketch.getSelectedDocument()
-// const page = document.selectedPage
 
 const directory = path.dirname(document.path)
-// const contentFileName = 'content.csv'
 
 //Excel header
 var generatedFileData = []
 
 var duplicateKeys = 0
-// const excelHeader = new ExcelContent('key', 'en-US')
-// generatedFileData.push(excelHeader)
 
 export default function() {
 
@@ -29,20 +24,102 @@ export default function() {
     for (let page of document.pages) {
       //Don't add symbols page
       if (page.name != "Symbols") {
-        // console.log(page.name)
 
-        for (let layer of page.layers) {
-          // console.log(layer.name)
-          getLayers(layer)
-        }
+        getPageContent(page)
+
       }
     }
     saveToFile()
   } else {
     console.log("Document contains no pages")
+    sketch.UI.message("Document contains no pages")
   }
 }
 
+function getPageContent(page) {
+  console.log("getPageContent: ", page.name)
+
+  for (let layer of page.layers) {
+    // console.log(layer.name)
+    getLayers(layer)
+  }
+}
+
+function getLayers(layer) {
+  if (layer.layers) {
+    // console.log("still has layers")
+    for (let layer of layer.layers) {
+      getLayers(layer)
+    }
+  } else {
+    console.log(layer.name, layer.type)
+    switch(layer.type) {
+      case String(sketch.Types.SymbolInstance):
+        contentFromSymbolLayer(layer)
+        break
+      case String(sketch.Types.Text):
+        contentFromTextLayer(layer)
+        break
+      case String(sketch.Types.Artboard):
+        contentFromArtboardLayer(layer)
+        console.log("artboard")
+        break
+    }
+  }
+}
+
+function contentFromSymbolLayer(symbol) {
+  console.log("contentFromSymbolLayer")
+  // console.log(layer.name)
+  // console.log(layer)
+  for (let override of symbol.overrides) {
+    // console.log("override:")
+    // console.log(override)
+    if (override.property == "stringValue") {
+      console.log("stringValue")
+      console.log(symbol.id)
+      console.log(symbol.symbolId)
+      console.log(override.id)
+      // console.log(symbol.name)
+      // console.log(symbol)
+      console.log(override.id)
+      // console.log(override.path)
+
+      // if (symbol.id == 'F5DA5A57-72E6-4A02-8048-0827032405B7') {
+      //   console.log(symbol)
+      // }
+
+      console.log(layerNamesFromPath(override.path))
+
+      let key = symbol.name + constants.excelDivider + layerNamesFromPath(override.path)
+      let sketchID = path.join(symbol.id, override.path)
+      addToSheet(sketchID, key, override.value)
+    }
+  }
+}
+
+function contentFromTextLayer(layer) {
+  console.log("contentFromTextLayer")
+  addToSheet(layer.id, layer.name, layer.text)
+}
+
+function contentFromArtboardLayer(artboard) {
+  console.log("contentFromArtboardLayer")
+  console.log("artboard layers: " + artboard.layers.length)
+  for (let layer of artboard.layers) {
+    console.log(layer.name, layer.type)
+    switch(layer.type) {
+      case String(sketch.Types.SymbolInstance):
+        contentFromSymbolLayer(layer)
+        break
+      case String(sketch.Types.Text):
+        contentFromTextLayer(layer)
+        break
+    }
+  }
+}
+
+/*
 function getLayers(layer) {
   if (layer.layers) {
     // console.log("still has layers")
@@ -78,32 +155,34 @@ function getLayers(layer) {
     }
   }
 }
+*/
 
-function ExcelContent(key, value) {
+function ExcelContent(sketchID, key, value) {
+  this.sketchID = sketchID
   this.key = key
   this.value = value
 }
 
-function addToSheet(key, value) {
+function addToSheet(sketchID, key, value) {
   //check if key already exists
-  if (generatedFileData.filter(excelContent => (excelContent.key === key)).length) {
+  if (generatedFileData.filter(excelContent => (excelContent.sketchID === sketchID)).length) {
     //skip
     duplicateKeys += 1
   } else {
     //add to array
-    const keyValue = new ExcelContent(key, value)
+    let keyValue = new ExcelContent(sketchID, key, value)
     generatedFileData.push(keyValue)
   }
-  console.log("Adding to sheet: " + key, value)
+  console.log("Adding to sheet: " + sketchID, key, value)
 }
 
 function saveToFile() {
   var date = new Date()
   var dateFormat = date.getFullYear() + "" + (date.getMonth() + 1) + "" + date.getDate()
-  console.log(dateFormat)
-  var sketchFileName = "Sketch"
-  var defaultPath = path.join(directory, 'sketchFileName-content-'+ dateFormat +'.xlsx')
-  console.log(defaultPath)
+
+  let name = path.basename(document.path, '.sketch');
+  let contentFileName = name + '-content-' + dateFormat + '.xlsx'
+  var defaultPath = path.join(path.dirname(document.path), contentFileName)
 
   var filePath = dialog.showSaveDialog({
     filters: [
@@ -111,16 +190,21 @@ function saveToFile() {
     ],
     defaultPath: defaultPath
   })
-  console.log(filePath);
-  console.log(generatedFileData);
-  const book = XLSX.utils.book_new()
-  const sheet = XLSX.utils.json_to_sheet(generatedFileData)
-  XLSX.utils.book_append_sheet(book, sheet, "content");
 
-  const content = XLSX.write(book, { type: 'buffer', bookType: 'xlsx', bookSST: false });
-  fs.writeFileSync(filePath, content, { encoding: 'binary' });
-  console.log("File created.")
-  onComplete()
+  //check if user want to save the file
+  if (filePath) {
+
+    // console.log(generatedFileData)
+    const book = XLSX.utils.book_new()
+    const sheet = XLSX.utils.json_to_sheet(generatedFileData)
+    XLSX.utils.book_append_sheet(book, sheet, "content");
+
+    const content = XLSX.write(book, { type: 'buffer', bookType: 'xlsx', bookSST: false });
+    fs.writeFileSync(filePath, content, { encoding: 'binary' });
+    console.log("File created as:", filePath)
+    onComplete()
+  }
+
 }
 
 function onComplete() {
