53a2b361902d258f6953464f1861d5400b67634d
wousser <wousser@gmail.com>
2018-11-22 21:55:51 +0800

    eslint standard

--- src/sync-page.js
+++ src/sync-page.js
@@ -12,8 +12,8 @@ const XLSX = require('xlsx')
 
 const document = sketch.getSelectedDocument()
 const directory = path.dirname(document.path)
-const defaultContentFileCSV = path.join(directory, "content.csv")
-const defaultContentFileXLSX = path.join(directory, "content.xlsx")
+const defaultContentFileCSV = path.join(directory, 'content.csv')
+const defaultContentFileXLSX = path.join(directory, 'content.xlsx')
 
 var contentDictionary = {}
 var languageOptions = []
@@ -26,50 +26,51 @@ if (fs.existsSync(defaultContentFileXLSX)) {
   contentFile = defaultContentFileCSV
 }
 
-export function syncAllPages(context) {
-  console.log("syncAllPages")
+export function syncAllPages (context) {
+  console.log('syncAllPages')
 }
 
-export function syncCurrentPage(context) {
-  console.log("syncCurrentPage")
+export function syncCurrentPage (context) {
+  console.log('syncCurrentPage')
 
-  //check if default file exist or ask for file input
+  // check if default file exist or ask for file input
   if (fs.existsSync(contentFile)) {
-    console.log("file exists: " + contentFile);
+    console.log('file exists: ' + contentFile)
   } else {
-    console.log("showOpenDialog");
+    console.log('showOpenDialog')
     var filePaths = dialog.showOpenDialog({
       properties: ['openFile'],
       defaultPath: 'directory',
-      filters: [
-        { name: 'Excel or CSV', extensions: ['xlsx', 'csv'] }
-      ]
+      filters: [{
+        name: 'Excel or CSV',
+        extensions: ['xlsx', 'csv']
+      }]
     })
     if (filePaths.length) {
       contentFile = filePaths[0]
     } else {
-      console.log("no file selected")
-      sketch.UI.message("No file selected. Select an Excel or CSV file to continue.")
+      console.log('no file selected')
+      sketch.UI.message('No file selected. Select an Excel or CSV file to continue.')
     }
   }
 
-  //check filetype
-  var fileType = contentFile.split(".").pop()
+  // check filetype
+  var fileType = contentFile.split('.').pop()
 
-  switch(fileType.toLowerCase()) {
-    case "csv":
-      console.log("csv")
+  switch (fileType.toLowerCase()) {
+    case 'csv':
+      console.log('csv')
       loadCSVData(contentFile)
       populatePage()
       break
-    case "xls", "xlsx":
-      console.log("Excel")
+    case 'xls', 'xlsx':
+      console.log('Excel')
       loadExcelData(contentFile)
       populatePage()
       break
     default:
-      console.log("File format not supported.")
-      sketch.UI.message("File format not supported.")
+      console.log('File format not supported.')
+      sketch.UI.message('File format not supported.')
   }
 }
 
@@ -85,71 +86,73 @@ function showLanguageSelectionPopup(languageOptions) {
     console.log(selectedLanguage)
   } else {
     console.log("showLanguageSelectionPopup() pressed cancel.")
-    return
+    
   }
 }
 
-function loadExcelData(contentFile) {
+function loadExcelData (contentFile) {
   const xlsData = fs.readFileSync(contentFile)
 
-  var workbook = XLSX.read(xlsData, {type: "buffer"})
+  var workbook = XLSX.read(xlsData, {
+    type: 'buffer'
+  })
   /* Get worksheet. Only support one sheet at the moment. */
   var first_sheet_name = workbook.SheetNames[0]
   var worksheet = workbook.Sheets[first_sheet_name]
 
   var excelJson = XLSX.utils.sheet_to_json(worksheet)
 
-  let rowNumber = 2 //Excel row starts at 0, and 1st row is key/value
+  let rowNumber = 2 // Excel row starts at 0, and 1st row is key/value
 
-  //get language options
-  var keyAndLanguageOptions = Object.keys(excelJson[0]) //get language options from first row
+  // get language options
+  var keyAndLanguageOptions = Object.keys(excelJson[0]) // get language options from first row
   if (!keyAndLanguageOptions) {
-    console.log("File format not supported. Language options not found")
-    sketch.UI.message("File format not supported. Language options not found")
+    console.log('File format not supported. Language options not found')
+    sketch.UI.message('File format not supported. Language options not found')
     return
   }
-  keyAndLanguageOptions.shift() //remove 'key'
+  keyAndLanguageOptions.shift() // remove 'key'
   for (let language of keyAndLanguageOptions) {
     languageOptions.push(language)
   }
 
-  //ask for language first so we don't load all language data into the object.
+  // ask for language first so we don't load all language data into the object.
   showLanguageSelectionPopup(languageOptions)
   if (!selectedLanguage) {
-    console.log("loadExcelData() aborted. No language selected.")
+    console.log('loadExcelData() aborted. No language selected.')
     return
   }
 
   for (var row in excelJson) {
-        //skip empty content
-        if (excelJson[row][selectedLanguage]) {
-          console.log("rowNumber: " + rowNumber)
-          contentDictionary[String(excelJson[row]['key'])] = String(excelJson[row][selectedLanguage])
-        } else {
-          console.log("skipped rowNumber: " + rowNumber)
-        }
-        rowNumber += 1
+    // skip empty content
+    if (excelJson[row][selectedLanguage]) {
+      console.log('rowNumber: ' + rowNumber)
+      contentDictionary[String(excelJson[row]['key'])] = String(excelJson[row][selectedLanguage])
+    } else {
+      console.log('skipped rowNumber: ' + rowNumber)
     }
-    onComplete()
+    rowNumber += 1
+  }
+  onComplete()
 }
 
-function populatePage(page) {
-  //abort if no language is chosen
+function populatePage (page) {
+  // abort if no language is chosen
   if (!selectedLanguage) {
-    console.log("populatePage() aborted. No language selected.")
+    console.log('populatePage() aborted. No language selected.')
     return
   }
 
   // Use selected page if no page is set
   if (!page) {
     page = document.selectedPage
-    console.log("page: " + page.name)
+    console.log('page: ' + page.name)
   }
 
-  console.log("page layers: " + page.layers.length)
+  console.log('page layers: ' + page.layers.length)
   for (let layer of page.layers) {
     console.log(layer.name, layer.type)
-    switch(layer.type) {
+    switch (layer.type) {
       case String(sketch.Types.SymbolInstance):
         updateSymbolLayer(layer)
         break
@@ -158,7 +161,7 @@ function populatePage(page) {
         break
       case String(sketch.Types.Artboard):
         updateArtboardLayer(layer)
-        console.log("artboard");
+        console.log('artboard')
         break
     }
   }
@@ -166,49 +169,48 @@ function populatePage(page) {
   onComplete()
 }
 
-//Load CSV File
-function loadCSVData(contentFile) {
+// Load CSV File
+function loadCSVData (contentFile) {
   const csvData = fs.readFileSync(contentFile)
 
   csv({
     noheader: false
     // output: "csv"
   })
-  .fromString(csvData.toString())
-  .subscribe((json,lineNumber)=>{
-    console.log(lineNumber)
-    // console.log(json)
-    updateContent(json['key'], json[selectedLanguage])
-
-}, onError, onComplete)
+    .fromString(csvData.toString())
+    .subscribe((json, lineNumber) => {
+      console.log(lineNumber)
+      // console.log(json)
+      updateContent(json['key'], json[selectedLanguage])
+    }, onError, onComplete)
 }
 
-function onError(err) {
-    console.log("Error: " + err)
-    sketch.UI.message("An error occured: " + err)
+function onError (err) {
+  console.log('Error: ' + err)
+  sketch.UI.message('An error occured: ' + err)
 }
 
-function onComplete() {
-  console.log("Completed")
-  sketch.UI.message("Completed")
+function onComplete () {
+  console.log('Completed')
+  sketch.UI.message('Completed')
 }
 
-function updateTextLayer(layer) {
-  console.log("updateTextLayer")
+function updateTextLayer (layer) {
+  console.log('updateTextLayer')
   console.log(layer.name)
   if (contentDictionary[layer.name]) {
     layer.text = contentDictionary[layer.name]
   }
-  console.log("updateTextLayer done")
+  console.log('updateTextLayer done')
 }
 
-function updateSymbolLayer(symbol) {
-  console.log("updateSymbolLayer")
+function updateSymbolLayer (symbol) {
+  console.log('updateSymbolLayer')
   console.log(symbol.name)
   // console.log(symbol)
 
   for (let override of symbol.overrides) {
-    if (override.property == "stringValue") {
+    if (override.property == 'stringValue') {
       let layerNameAndOverride = symbol.name + constants.excelDivider + layerNamesFromPath(override.path)
 
       if (contentDictionary[layerNameAndOverride]) {
@@ -216,15 +218,15 @@ function updateSymbolLayer(symbol) {
       }
     }
   }
-  console.log("updateSymbolLayer done")
+  console.log('updateSymbolLayer done')
 }
 
-function updateArtboardLayer(artboard) {
-  console.log("updateArtboardLayer")
-  console.log("page layers: " + artboard.layers.length)
+function updateArtboardLayer (artboard) {
+  console.log('updateArtboardLayer')
+  console.log('page layers: ' + artboard.layers.length)
   for (let layer of artboard.layers) {
     console.log(layer.name, layer.type)
-    switch(layer.type) {
+    switch (layer.type) {
       case String(sketch.Types.SymbolInstance):
         updateSymbolLayer(layer)
         break
@@ -233,26 +235,24 @@ function updateArtboardLayer(artboard) {
         break
     }
   }
-  console.log("updateArtboardLayer done")
+  console.log('updateArtboardLayer done')
 }
 
 // **********************
 //   Helper methods
 // **********************
-//TODO: function duplicated
-function layerNamesFromPath(path) {
+// TODO: function duplicated
+function layerNamesFromPath (path) {
+  var layerNames = []
+  let layerIDs = path.split(constants.sketchSymbolDivider)
+  for (let layerID of layerIDs) {
+    let layer = document.getLayerWithID(layerID)
 
-    var layerNames = []
-    let layerIDs = path.split(constants.sketchSymbolDivider)
-    for (let layerID of layerIDs) {
-
-      let layer = document.getLayerWithID(layerID)
-
-      //TODO: Sketch libraries not supported yet.
-      if (layer) {
-        let layerName = layer.name
-        layerNames.push(layerName)
-      }
+    // TODO: Sketch libraries not supported yet.
+    if (layer) {
+      let layerName = layer.name
+      layerNames.push(layerName)
     }
-    return layerNames.join(constants.excelDivider)
+  }
+  return layerNames.join(constants.excelDivider)
 }
